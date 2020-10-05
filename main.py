025cf01407afc201b9051143fdc90d4c8667b6cb
Ade-StapleHill <ade.perry@digilocal.org.uk>
2020-10-05 19:15:56 +0100

    Work in progress. Detect if ball hits top or bottom of bat.

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
-
-import pygame, sys, math, random
+import pygame, math, random
 from pygame.locals import *
 
 screen = pygame.display.set_mode((400, 600))
 
+dbg_flag = True     ## set True for DBG print
+def dbg_print(str):
+    global dbg_flag
+
+    if dbg_flag:
+        print(f"DBG: {str}")
+
+
+
+#https://gamedev.stackexchange.com/questions/61705/pygame-colliderect-but-how-do-they-collide
+
+def on_collide(paddle, ball, nregions=3):
+    # we collided, so which side? Args are the `Rect`s of paddle and ball.
+    # Divide width of paddle into nregions, default 3, and return index 
+    #
+    print("paddle.centery=%d, ball.centery=%d " % (paddle.centery, ball.centery))
+    if paddle.centery < ball.centery:
+        dbg_print("paddle bottom")
+    elif paddle.centery > ball.centery:
+        dbg_print("paddle top")
+
+    if paddle.centerx < ball.centerx:
+        dbg_print("paddle right")
+    elif paddle.centerx > ball.centerx:
+        dbg_print("paddle left")
+
+    ball_offset = ball.centerx - paddle.x
+    region_width = int(paddle.width / nregions)
+    region = int(ball_offset / region_width)
+    if region < 0: region = 0 ## left edge clipped the ball
+    if region >= nregions : region = nregions - 1  ## right edge clipped ball
+    dbg_print(f"region={region}, ball_offset={ball_offset}, nregions={nregions}, paddle.width={paddle.width}")
+
+    return region
+
 # Making the bat
+
 
 class Bat:
     def __init__(self):
         self.x = screen.get_width() / 2
         self.y = screen.get_height() - 20
+        self.drawn = None
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
@@ -19,48 +55,71 @@ class Bat:
             self.x += 7
 
     def draw(self):
-        pygame.draw.line(screen, (0, 0, 0), (self.x - 30, self.y), (self.x + 30, self.y), 6)
+        self.drawn = pygame.draw.line(screen, (0, 0, 0), (self.x - 30, self.y),
+                         (self.x + 30, self.y), 6)
+
 
 # Defining the ball
+
 
 class Ball:
     def __init__(self):
         self.direction = math.pi + math.pi / 3 * random.random()
-        self.speed = 1
-        self.dx = math.sin(self.direction) * self.speed
-        self.dy = math.cos(self.direction) * self.speed
+        self.speed = 4
+        self.move_vector()
         self.x = screen.get_width() / 2
         self.y = screen.get_height() - 60
         self.r = 10
         self.colour = (0, 0, 0)
+        self.drawn = None
+        self.hit = False
+
+    def move_vector(self):
+        self.dx = math.sin(self.direction) * self.speed
+        self.dy = math.cos(self.direction) * self.speed
+
     def move(self):
         self.x += self.dx
         self.y += self.dy
+
     def draw(self):
-        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.r, 0) 
+        self.drawn = pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)),
+                           self.r, 0)
+
     def bounce(self, blocks, score):
 
         # Bouncing off the sides
 
-        if (self.x <= 20 and self.dx < 0) or (self.x >= screen.get_width() - 20 and self.dx > 0):
+        if (self.x <= 20 and self.dx < 0) or (self.x >= screen.get_width() - 20
+                                              and self.dx > 0):
             self.dx *= -1
         if self.y <= 10:
             self.dy *= -1
 
         # Bounce off the bat
+        spin_arr = (-10, -5, 0, 0, 0, 5, 10)
+        if bat.drawn.colliderect(self.drawn) and not self.hit:
+            self.hit = True   ## avoid multiple collision as ball travels accross
 
-        if pygame.Rect(bat.x-30, bat.y, 60, 10).colliderect(self.x - 10, self.y - 10, 20, 20):
-            self.dy *= -1
+            if (bat.drawn.centery > self.drawn.centery):
+                # collide top of bat
+                region = on_collide(bat.drawn, self.drawn, len(spin_arr))
+                self.dy *= -1   ## change vertical direction
 
-            # Reverse direction if near the edge of the bat
+                spin_angle = spin_arr[region]
+                if spin_angle:
+                    self.direction = math.atan2(self.dy, self.dx) + math.radians(spin_angle)
+                    self.move_vector()
 
-            if self.x - bat.x < -20 or self.x - bat.x > 20:
-                self.dx *= -1
-        
+                dbg_print(f"{spin_angle= }, {self.direction= }") 
+        else:
+            self.hit = False    ## reset to detect new collision
+
         # Bounce off the blocks
 
         for block in blocks:
-            if pygame.Rect(block.x, block.y, 50, 20).colliderect(self.x - 10, self.y - 10, 20, 20):
+            if pygame.Rect(block.x, block.y, 50, 20).colliderect(
+                    self.x - 10, self.y - 10, 20, 20):
                 self.dy *= -1
                 blocks.remove(block)
                 score += 10
@@ -71,25 +130,33 @@ class Ball:
 
         for block in blocks:
             if Block.colour == (255, 0, 0):
-                if pygame.Rect(block.x, block.y, 50, 20).colliderect(self.x - 10, self.y - 10, 20, 20):
+                if pygame.Rect(block.x, block.y, 50, 20).colliderect(
+                        self.x - 10, self.y - 10, 20, 20):
                     score += 40
 
 
-
 # Defining the blocks
+
 
 class Block:
     def __init__(self, x, y, colour):
         self.x = x
         self.y = y
         self.colour = colour
+
     def draw(self):
         pygame.draw.rect(screen, self.colour, Rect(self.x, self.y, 50, 20), 0)
 
+
 # Defining what to do when the game finishes
 
+
 def gameOver():
-    pygame.draw.rect(screen, (255, 255, 255), Rect(10, 10, screen.get_width() - 20, screen.get_height() - 20), 0)
+    pygame.draw.rect(
+        screen, (255, 255, 255),
+        Rect(10, 10,
+             screen.get_width() - 20,
+             screen.get_height() - 20), 0)
 
     txt = font.render("Score", True, (0, 0, 0))
     screen.blit(txt, (int(screen.get_width() / 2 - txt.get_width() / 2), 50))
@@ -108,6 +175,7 @@ def gameOver():
                 pygame.quit()
                 raise SystemExit
 
+
 # Initialize pygame
 
 pygame.init()
@@ -120,7 +188,6 @@ score = 0
 font = pygame.font.Font(None, 30)
 bat = Bat()
 blocks = []
-
 
 # Block colours
 
@@ -168,7 +235,11 @@ while True:
 
     # Border
 
-    pygame.draw.rect(screen, (255, 255, 255), Rect(10, 10, screen.get_width() - 20, screen.get_height() - 20), 0)
+    pygame.draw.rect(
+        screen, (255, 255, 255),
+        Rect(10, 10,
+             screen.get_width() - 20,
+             screen.get_height() - 20), 0)
 
     # Draw the blocks
 
@@ -197,7 +268,6 @@ while True:
             for x in range(25, 360, 60):
                 blocks.append(Block(x, y, colours[i]))
             i += 1
-        
 
     # Update the display
 
